b724a46af5e1c0d72c334f6aa9103f47be088eaf
Elnaraa9 <7khtht32code.edu.az>
2022-05-20 11:52:25 +0400

    uncompleted task

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
