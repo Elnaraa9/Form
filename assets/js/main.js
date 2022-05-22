4a335ebae46774e5b3695102f8f4e8186620d9a8
Elnaraa9 <100670042+Elnaraa9@users.noreply.github.com>
2022-05-22 16:56:54 +0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,41 @@
-function showError(input, message) {
-	return showMessage(input, message, false);
-}
+btn = document.querySelector(".button");
+btn.onclick = function(){
+    let named = document.querySelector(".name").value;
+    let surname = document.querySelector(".surname").value;
+    let email = document.querySelector(".email").value;
+    let age = parseInt(document.querySelector(".age").value);
+    let namevalid = false;
+    let surnamevalid = false;
+    let emailvalid = false;
+    let agevalid = false;
+    let valid = false;
+    if(named !== "" && named !== " " && named[0] === named[0].toUpperCase() && named.length > 3){
+        namevalid = true;
+    }
+    else{
+        alert("İnvalid")
+    }
+    if(surname !== "" && surname !== " " && surname[0] === surname[0].toUpperCase() && surname.length > 3){
+        surnamevalid = true;
+    }
 
-function showSuccess(input) {
-	return showMessage(input, "", true);
-}
+    for (const element of email) {
+        if(email !== "" && email !== " " && element === "@" && email === email.toLowerCase()){
+            emailvalid = true;
+        }
+		else{
+			alert('Invalid email')
+		}
+    }
 
-function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
-}
+    if(age > 0){
+        agevalid = true;
+    }
+    else{
+        alert('İnvalid age')
+    }
+
+    if (namevalid && surnamevalid && emailvalid && agevalid) {
+        valid = true;
+    } 
+};
